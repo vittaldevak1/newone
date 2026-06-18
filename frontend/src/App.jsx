eaed05b7557718396aeb95ef99b37c647f6e413c
Dhevesh <alwaysbp1@gmail.com>
2026-06-18 23:31:50 +0530

    adding react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>My First React Project 🚀</h1>
+      <p>I finally got React running!</p>
+    </div>
+  );
+}
+
+export default App;
